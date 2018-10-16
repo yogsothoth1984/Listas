6eaaf323bd8ad19c1a22bd5d3078b51d74022eea
max <40814622+vloxomire@users.noreply.github.com>
2018-10-16 09:25:54 -0300

    0925

--- app/src/main/java/com/example/listas/MainActivity.java
+++ app/src/main/java/com/example/listas/MainActivity.java
@@ -2,14 +2,18 @@ package com.example.listas;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     private ListView listView;
+    private List<String> nombres;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,7 +22,7 @@ public class MainActivity extends AppCompatActivity {
 
         listView=findViewById(R.id.listViewId);
 
-        List<String> nombres =  new ArrayList<String>();
+        nombres =  new ArrayList<String>();
         nombres.add("Max");
         nombres.add("Christian");
         nombres.add("Eric");
@@ -27,6 +31,14 @@ public class MainActivity extends AppCompatActivity {
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,nombres);
         //Enlazamos el adaptador con nuestro listView
         listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(MainActivity.this,"Clickeado: "+nombres.get(position),
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
 
